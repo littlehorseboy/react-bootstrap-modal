377e6b95e71c0b8d140008800c07677f6292eca6
horse <littlehorseboy@gmail.com>
2019-03-22 11:22:15 +0800

    feat: Modal.test.js

--- src/components/UI/Modal/Modal.test.js
+++ src/components/UI/Modal/Modal.test.js
@@ -4,7 +4,7 @@ import toJSON from 'enzyme-to-json';
 import { ThemeProvider } from 'react-jss';
 import Modal from './Modal.jsx';
 
-test('Modal component renders correctly', () => {
+describe('Modal.jsx', () => {
   // 自定義一個 theme
   const theme = {
     shadows: [],
@@ -18,12 +18,41 @@ test('Modal component renders correctly', () => {
         zIndex={1}
         changeModalVisible={() => {}}
         title="TEST"
-        content={<div>TEST</div>}
+        content={<div>This is content.</div>}
         position={{ top: '250px', left: '250px' }}
       />
     </ThemeProvider>,
   );
-  const wrapperJson = toJSON(wrapper);
 
-  expect(wrapperJson).toMatchSnapshot();
+  test('Modal component renders correctly', () => {
+    const wrapperJson = toJSON(wrapper);
+    expect(wrapperJson).toMatchSnapshot();
+  });
+
+  test('Modal => .modal-title textContent is `TEST`', () => {
+    expect(wrapper.find('.modal-title').text()).toBe('TEST');
+  });
+
+  test('Modal => .modal-body innerHTML is `<div>This is content.</div>`', () => {
+    expect(wrapper.find('.modal-body').children().html()).toBe('<div>This is content.</div>');
+  });
+
+  test('Modal => .modal-body innerHTML is `<div>This is content.</div>`', () => {
+    expect(wrapper.find('.modal-dialog').hasClass('hidden')).toBeFalsy();
+  });
+
+  test('Modal => .modal-body innerHTML is `<div>This is content.</div>`', () => {
+    wrapper.setProps({
+      children: <Modal
+        open={false}
+        zIndex={1}
+        changeModalVisible={() => {}}
+        title="TEST"
+        content={<div>This is content.</div>}
+        position={{ top: '250px', left: '250px' }}
+      />,
+    });
+
+    expect(wrapper.find('.modal-dialog').hasClass('hidden')).toBeTruthy();
+  });
 });
